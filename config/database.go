35e3803a51c87511fce92ac091dc4c753549988f
Logan <logan.cc@outlook.com>
2024-01-07 10:09:26 -0500

    habilitando ssl

--- config/database.go
+++ config/database.go
@@ -11,13 +11,12 @@ import (
 
 func ConectaDB() *gorm.DB {
 	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN: fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=%s",
+		DSN: fmt.Sprintf("user=%s password=%s dbname=%s host=%s",
 
 			os.Getenv("DB_USER"),
 			os.Getenv("DB_PASSWORD"),
 			os.Getenv("DB_NAME"),
-			os.Getenv("DB_HOST"),
-			os.Getenv("DB_SSL_MODE")),
+			os.Getenv("DB_HOST")),
 
 		PreferSimpleProtocol: true,
 	}), &gorm.Config{})
