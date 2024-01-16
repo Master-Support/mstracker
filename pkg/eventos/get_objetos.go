f2a2c899dc042e49c1232cffa293c24021795187
Logan <logan.cc@outlook.com>
2024-01-16 08:47:13 -0300

    feat: utlizando query SQL para retornar no GET apenas objetos entregues ao detinatário.

--- pkg/eventos/get_objetos.go
+++ pkg/eventos/get_objetos.go
@@ -7,22 +7,39 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func (h handler) GetObjetos(ctx *gin.Context) {
-	var envios []models.StatusObjeto  // Renomeie a variável para 'envios'
+// func (h handler) GetObjetos(ctx *gin.Context) {
+// 	var envios []models.StatusObjeto
+// 	var objetos_entrgues []models.StatusObjeto
 
-	if result := h.DB.Find(&envios); result.Error != nil {
+// 	if result := h.DB.Find(&envios); result.Error != nil {
+// 		ctx.AbortWithError(http.StatusNotFound, result.Error)
+// 		return
+// 	}
+
+// 	for _, envio := range envios {
+// 		if envio.StatusObjeto != "Objeto entregue ao destinatário"{
+// 			objetos_entrgues = append(objetos_entrgues, envio)
+// 			//quando um objeto já estiver sido entregue ele ira para outra categoria de objetos entregues.
+// 			//caso ao contrario o objeto será exibido em envios
+// 		}
+// 	}
+
+// 	ctx.JSON(http.StatusOK, gin.H{"envios": envios})
+// }
+
+func (h handler) GetObjetos(ctx *gin.Context) {
+	var envios []models.StatusObjeto
+	var objetos_entregues []models.StatusObjeto
+
+	// Exemplo de consulta SQL usando Raw
+	query := "SELECT * FROM status_objetos WHERE status_objeto = 'Objeto entregue ao destinatário'"
+	result := h.DB.Raw(query).Scan(&objetos_entregues)
+	if result.Error != nil {
 		ctx.AbortWithError(http.StatusNotFound, result.Error)
 		return
 	}
 
-	for _, envio := range envios {
-		if envio.StatusObjeto != "Objeto entregue ao destinatário"{
-			//quando um objeto já estiver sido entregue ele ira para outra categoria de objetos entregues.
-			//caso ao contrario o objeto será exibido em envios
-			return 
-		}
-	}
+	// Restante do código...
 
-
-	ctx.JSON(http.StatusOK, gin.H{"envios": envios})  // Use "envios" como chave no JSON
+	ctx.JSON(http.StatusOK, gin.H{"envios": envios, "objetos_entregues": objetos_entregues})
 }
