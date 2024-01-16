55c49a8ea361766a6e5a0853b7ce7caeda3e6f1b
Logan <logan.cc@outlook.com>
2024-01-16 08:48:27 -0300

    refector: removenod idObjeto para usar id padraão gorm.

--- models/status_objeto.go
+++ models/status_objeto.go
@@ -4,7 +4,6 @@ import "gorm.io/gorm"
 
 type StatusObjeto struct {
 	gorm.Model
-	IdObjeto              uint64 `json:"idObjeto" gorm:"primaryKey"`
 	CodigoObjeto          string `json:"codigoObjeto" gorm:"unique;not null"`
 	NomeObjeto            string `json:"nomeObjeto" gorm:"not null"`
 	DataPrevistaDeEntrega string `json:"dataPrevistaDeEntrega"`
